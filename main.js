86795ed27b9f995292df9fb20f4a6c885815ab6e
Ramon Nascimento <ramonwordads@hotmail.com>
2023-02-25 12:38:43 -0300

    update struct for

--- main.js
+++ main.js
@@ -8,21 +8,15 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 
 
 
-let contador = 0
 
-while(contador < listaDeTeclas.length){
+for (let contador = 0; contador < listaDeTeclas.length; contador++){
 
     const tecla = listaDeTeclas[contador]
     const instrumento = tecla.classList[1];
-
-    
     const idAudio = `#som_${instrumento}`
     
-
     tecla.onclick = function() {
         tocaSom(idAudio)
     }
 
-    contador++;
-    
 }
